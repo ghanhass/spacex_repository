a770b63986bf593cd4cca7b0ce83fe2b9ebf446b
ghanhass <ghanmi.hassen91@gmail.com>
2026-05-26 20:56:22 +0100

    Implementing loading progress bar and enhabncing launch listings scrollability

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { Store } from '@ngrx/store';
+import { map, Observable } from 'rxjs';
+import { LaunchState } from './state/launch.reducer';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatProgressBarModule, AsyncPipe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('spaceX_kata');
+  isLoading$: Observable<boolean>;
+
+  constructor(private store: Store<{ launch: LaunchState }>) {
+    this.isLoading$ = this.store.select((state) => state.launch).pipe(
+      map((res) => res.loading)
+    );
+  }
 }
