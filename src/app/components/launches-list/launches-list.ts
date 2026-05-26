be3391aaab183d66be547ca6e84f13a3d0defd66
ghanhass <ghanmi.hassen91@gmail.com>
2026-05-26 20:40:50 +0100

    Launches list component, template and styling updates and fixes

--- src/app/components/launches-list/launches-list.ts
+++ src/app/components/launches-list/launches-list.ts
@@ -1,30 +1,44 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, computed, inject, model, OnInit, signal, Signal, WritableSignal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { SpacexService } from '../../services/spacex';
-import { Subscription } from 'rxjs';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { Store } from '@ngrx/store';
+import { LaunchState } from '../../state/launch.reducer';
+import { loadLaunches } from '../../state/launch.actions';
+import { Launch } from '../../interfaces/interfaces';
+import { selectAllLaunches } from '../../state/launch.selectors';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-launches-list',
   standalone: true,
   imports: [CommonModule, FormsModule, MatCardModule, MatFormFieldModule, MatInputModule, MatIconModule, MatChipsModule, MatButtonModule],
-  templateUrl: './launches-list.html'
+  templateUrl: './launches-list.html',
+  styleUrl: "./launches-list.scss"
 })
 export class LaunchesListComponent implements OnInit {
-  allLaunches: any[] = [];
-  filteredLaunches: any[] = [];
-  searchTerm: string = '';
-  
-  private sub! : Subscription; 
+  allLaunches: Signal<Launch[]>;
+  filteredLaunches: Signal<Launch[]>;
 
-  constructor(private spacexService: SpacexService) {}
+  searchTerm: WritableSignal<string>  = model('');
+
+  constructor(private store: Store<{ launch: LaunchState }>, private router: Router) {
+    this.allLaunches = this.store.selectSignal(selectAllLaunches);
+
+    this.filteredLaunches = computed(() => {
+      let filteredList = this.store.selectSignal(selectAllLaunches)().filter(launch =>
+        ( launch.name.toUpperCase().trim() ).includes(this.searchTerm().toUpperCase().trim())
+      );
+      return filteredList;
+    });
+    
+  }
 
 
   ngOnInit(): void {
@@ -32,15 +46,11 @@ export class LaunchesListComponent implements OnInit {
   }
 
   loadLaunches() {
-    this.sub = this.spacexService.getPastLaunches().subscribe(data => {
-      this.allLaunches = data;
-      this.filteredLaunches = data;
-    });
+    this.store.dispatch(loadLaunches());
   }
 
-  onSearchChange() {    
-    this.filteredLaunches = this.allLaunches.filter(launch => 
-      launch.name.includes(this.searchTerm)
-    );
+  goToLaunch(launch: Launch){
+    let id: string = launch.id;
+    this.router.navigate(["launch",id])
   }
 }
