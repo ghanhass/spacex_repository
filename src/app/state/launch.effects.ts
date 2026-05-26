178a5305e6d945d8a512e32c8858aad0902cf54a
ghanhass <ghanmi.hassen91@gmail.com>
2026-05-26 20:53:54 +0100

    Ensuring typing in actions, and implementings the store effects

--- src/app/state/launch.effects.ts
+++ src/app/state/launch.effects.ts
@@ -1,7 +1,34 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { SpacexService } from '../services/spacex';
+import { catchError, exhaustMap, map, of } from 'rxjs';
+import { Launch } from '../interfaces/interfaces';
+import * as appActions from '../state/launch.actions';
 
 
 @Injectable()
 export class LaunchEffects {
+    private spaceXService = inject(SpacexService);
+    private actions$ = inject(Actions);
 
+    loadLaunches$ = createEffect(() => {
+        return this.actions$.pipe(
+            ofType(appActions.loadLaunches),
+            exhaustMap(() =>
+                this.spaceXService.getPastLaunches().pipe(
+                    map((res: Launch[]) => appActions.loadLaunchesSuccess({
+                        launches: res
+                    })),
+
+                    catchError((errorRes: string) =>
+                        of(
+                            appActions.loadLaunchesFailure({
+                                error: errorRes
+                            })
+                        )
+                    )
+                )
+            )
+        )
+    });
 }
