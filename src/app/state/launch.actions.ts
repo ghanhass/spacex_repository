ae3b84bcc7c4d8d21374dea1921839c66c13afd1
ghanhass <ghanmi.hassen91@gmail.com>
2026-05-27 15:18:15 +0100

    adding a store action and a reducer for the launch-detail

--- src/app/state/launch.actions.ts
+++ src/app/state/launch.actions.ts
@@ -4,3 +4,5 @@ import { Launch } from '../interfaces/interfaces';
 export const loadLaunches = createAction('[Launch List] Load Launches');
 export const loadLaunchesSuccess = createAction('[Launch List] Load Success', props<{ launches: Launch[] }>());
 export const loadLaunchesFailure = createAction('[Launch List] Load Failure', props<{ error: string }>());
+
+export const loadLaunchDetail = createAction("[Launch] Load Launch Details", props<{launchId: string}>());
--- src/app/state/launch.effects.ts
+++ src/app/state/launch.effects.ts
@@ -11,6 +11,24 @@ export class LaunchEffects {
     private spaceXService = inject(SpacexService);
     private actions$ = inject(Actions);
 
+    private handleError(errorRes: { error: string | ProgressEvent }) {
+        console.log("errorRes = ", errorRes);
+        if (errorRes.error instanceof ProgressEvent) {
+            return of(
+                appActions.loadLaunchesFailure({
+                    error: "Unknown API error"
+                })
+            );
+        }
+        else {
+            return of(
+                appActions.loadLaunchesFailure({
+                    error: errorRes.error
+                })
+            );
+        }
+    }
+
     loadLaunches$ = createEffect(() => {
         return this.actions$.pipe(
             ofType(appActions.loadLaunches),
@@ -26,6 +44,24 @@ export class LaunchEffects {
                                 error: errorRes
                             })
                         )
+                    )
+                )
+            )
+        )
+    });
+
+    loadLaunchDetail$ = createEffect(() => {
+        return this.actions$.pipe(
+            ofType(appActions.loadLaunchDetail),
+            exhaustMap((action) =>
+                this.spaceXService.getLaunchById(action.launchId).pipe(
+                    map((res: Launch) => appActions.loadLaunchesSuccess({
+                        launches: [res]
+                    })),
+
+                    catchError((errorRes: { error: string | ProgressEvent }) => {
+                        return this.handleError(errorRes);
+                    }
                     )
                 )
             )
