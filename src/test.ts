34c9ab9a6b2c4c1a26f81b89d7891cbd27d58e86
ghanhass <ghanmi.hassen91@gmail.com>
2026-05-28 17:58:45 +0100

    updating src/test.ts to simply include all .spec.ts files

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,4 @@
-// src/test.ts - Test entry point
+// Test entry point file
 import { getTestBed } from '@angular/core/testing';
 import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
 
@@ -12,6 +12,7 @@ getTestBed().initTestEnvironment(
   }
 );
 
-// Then, find all test files
-const context = require.context('./', true, /\.spec\.ts$/);
-context.keys().map(context);
+// Test files
+
+import "../src/app/components/launches-list/launches-list.spec";
+import "../src/app/components/launch-details/launch-details.spec";
