98696a6ab6b4a9ecf85c2896b93dd0bb72609e66
ghanhass <ghanmi.hassen91@gmail.com>
2026-05-28 19:08:02 +0100

    adding zone.js to the the environment to support fakeAsync in unit testing

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,7 @@
 // Test entry point file
 import { getTestBed } from '@angular/core/testing';
 import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
+import "zone.js/testing";
 
 // First, initialize the Angular testing environment
 getTestBed().initTestEnvironment(
@@ -13,6 +14,6 @@ getTestBed().initTestEnvironment(
 );
 
 // Test files
-
 import "../src/app/components/launches-list/launches-list.spec";
 import "../src/app/components/launch-details/launch-details.spec";
+import "../src/app/app.spec";
